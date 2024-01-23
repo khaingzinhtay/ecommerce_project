48fd21cb8946f6c145c2c09bb6d5464ba6c42422
Khaing Zin Htay <ms.khaingzin2003@gmail.com>
2024-01-23 21:02:10 +0630

    image update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -62,7 +62,8 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        //
+        $categories = Category::all();
+        return view('products.edit',compact('product','categories'));
     }
 
     /**
